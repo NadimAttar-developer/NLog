0718f4f61830041f871880721d97f231e05ee8a1
NadimAttar-developer <nadimattar.software.engineer@gmail.com>
2023-10-04 23:18:09 +0400

    first commit to the nlog project

---
+++ NLogAPI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace NLogAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
+{
+    //private readonly ILogger<HomeController> _logger;
+
+    Logger _logger = LogManager.GetLogger("NadimLogger");
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        try
+        {
+            _logger.Info("Hello, I'm Nadim Attar I'm working as a Software Engineer");
+
+            int x = 0;
+            int y = 1;
+            int z = y / x;
+
+            return StatusCode(200);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex.Message);
+
+            return StatusCode(500);
+        }
+    }
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# NLog
-NLog is a simple ASP.NET Web API project designed to demonstrate the fundamentals of integrating and using NLog, a versatile logging framework for .NET applications. This project provides a clear example of how to set up and configure NLog for efficient logging in your web applications.
